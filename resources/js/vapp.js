22a8ceb61034106742a634133ed884fa21f546a6
ssukhraj12 <ssukhraj12@gmail.com>
2026-02-13 15:59:25 +0530

    quill Editor

--- resources/js/vapp.js
+++ resources/js/vapp.js
@@ -4,6 +4,8 @@ import App from "./App.vue";
 import vuetify from "./vuetify.js";
 import router from "./router/index.js";
 import store from "./store/index.js";
+import { QuillEditor } from "@vueup/vue-quill";
+import "@vueup/vue-quill/dist/vue-quill.snow.prod.css"
 import VueApexCharts from "vue3-apexcharts";
 import Toast, {useToast} from "vue-toastification";
 import "vue-toastification/dist/index.css";
@@ -25,10 +27,32 @@ window.Echo = new Echo({
     forceTLS: true
 })
 
+import {Quill} from "@vueup/vue-quill";
+const Link = Quill.import('formats/link')
+
+class CustomLink extends Link {
+    static create(value) {
+        const node = super.create(value);
+
+        node.setAttribute('href', this.sanitize(value));
+        if (window.isAdminUser) { // Pass this flag globally before initializing
+            node.setAttribute('rel', 'dofollow');
+        } else {
+            node.setAttribute('rel', 'noreferrer noopener');
+        }
+        // Remove or change target attribute as needed
+        node.removeAttribute('target');
+
+        return node;
+    }
+}
+Quill.register('formats/link',CustomLink,true);
+
 const app = createApp(App);
 app.use(Toast, options);
 app.config.globalProperties.$toast = useToast();
 window.Toast = useToast();
+app.component('QuillEditor', QuillEditor);
 app.use(VueApexCharts);
 app.use(vuetify);
 app.use(router);
