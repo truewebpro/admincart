6dc054a8fd1a001842cade5f453d7c9455ea239c
ssukhraj12 <ssukhraj12@gmail.com>
2026-03-13 15:10:47 +0530

    intgration menu

--- resources/js/admin/integrate/IntegrateList.vue
+++ resources/js/admin/integrate/IntegrateList.vue
@@ -2,19 +2,19 @@
     <v-container class="pa-2">
         <v-row dense>
             <v-col cols="12" md="6">
-                <h2>Integration List</h2>
+                <h2>Integrations</h2>
             </v-col>
             <v-col cols="12" md="6"></v-col>
         </v-row>
-        <v-row v-for="(ilist,ldx) in ilists" :key="ldx" class="mb-4 bg-white elevation-3 rounded-shaped">
+        <v-row v-for="(ilist,ldx) in ilists" :key="ldx" class="mb-10 bg-white elevation-3 rounded-shaped">
             <v-col cols="12" md="12" class="pb-0">
-                <h2 class="text-decoration-underline">{{ilist.name}}</h2>
+                <h2 class="text-decoration-underline text-blue-darken-4"> <v-icon class="me-1">{{ilist.icon}}</v-icon> {{ilist.name}}</h2>
             </v-col>
             <v-col v-for="(partner,pdx) in ilist.partners" :key="pdx"  cols="12" md="4">
                 <v-card class="elevation-1 border-0 bg-blue-lighten-5">
                     <v-card-item :title="partner.title" appendIcon="mdi-dots-vertical"></v-card-item>
                     <v-card-text>
-                        <v-img :src="partner.logo" contain aspectRatio="16 / 9" max-height="100" min-height="100" maxHeight="100"/>
+                        <v-img :src="partner.logo" contain aspectRatio="16 / 9" max-height="60" min-height="60"/>
 
                     </v-card-text>
                     <v-card-actions>{{partner.desc}}</v-card-actions>
@@ -29,7 +29,7 @@ export default {
     data(){
         return {
             ilists:[
-                {name:'Shipping',partners:[
+                {name:'Shipping',icon:"mdi-truck-fast",partners:[
                         {name:'SendCloud',title:"Sendcloud",
                             desc:"Manage All couriers at 1 Place",
                             logo:"https://www.sendcloud.com/wp-content/uploads/2022/09/Sendcloud_small_rgb.svg"},
@@ -40,7 +40,7 @@ export default {
                             desc:"Manage DPD Domestic Account",
                             logo:"https://www.dpd.com/wp-content/themes/DPD_NoLogin/images/DPD_logo_redgrad_rgb_responsive.svg"},
                     ]},
-                {name:'Payments',partners:[
+                {name:'Payments',icon:"mdi-account-credit-card-outline",partners:[
                         {name:'StripeApp',title:"Stripe",
                             desc:"Managed Pay by all Debit/Credit Cards",
                             logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Stripe_Logo%2C_revised_2016.svg/1280px-Stripe_Logo%2C_revised_2016.svg.png"},
@@ -60,7 +60,7 @@ export default {
                             desc:"Direct payment in your Bank",
                             logo:"https://www.onlinepoker.net/wp-content/uploads/2018/05/wire-transfer-logo.png"},
                     ]},
-                {name:'Email',partners:[
+                {name:'Email',icon:"mdi-email-fast-outline",partners:[
                         {name:'MailTrap',title:"Mailtrap",
                             desc:"Manage all email campaigns with no Limit",
                             logo:"https://mailtrap.io/wp-content/uploads/2025/11/mailtrap-new-dark-2.svg"},
@@ -74,7 +74,7 @@ export default {
                             desc:"Send bulk email campaigns with sendgrid",
                             logo:"https://upload.wikimedia.org/wikipedia/commons/b/b6/SendGrid_2016_Logo.png"},
                     ]},
-                {name:'Hosting',partners:[
+                {name:'Hosting',icon:"mdi-web-sync",partners:[
                         {name:'AwsAmplify',title:"AWS Amplify",
                             desc:"Auto deploy frontend with CDN",
                             logo:"https://miro.medium.com/v2/1*EB-8t3dR5qMQYNXyqgM2wQ.png"},
