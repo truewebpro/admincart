196dbbe0b9bf0cd363fe21ff984207fa2c020c04
ssukhraj12 <ssukhraj12@gmail.com>
2026-08-01 12:36:04 +0530

    redis cache setup

--- config/cache.php
+++ config/cache.php
@@ -15,7 +15,8 @@ return [
     |
     */
 
-    'default' => env('CACHE_DRIVER', 'file'),
+//    'default' => env('CACHE_DRIVER', 'file'),
+    'default' => env('CACHE_STORE', env('CACHE_DRIVER', 'file')),
 
     /*
     |--------------------------------------------------------------------------
